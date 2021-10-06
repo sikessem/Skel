910eb93fc12ab110489f42a63979719f3cc04eea
SIGUI Kessé Emmanuel (https://sikessem.com) <developer@sikessem.com>
2021-10-06 22:50:19 +0200

    :zap: Improve file interpreting

--- lib/Skel/Interpreter.php
+++ lib/Skel/Interpreter.php
@@ -9,23 +9,19 @@ class Interpreter {
 
     protected Lexer $lexer;
 
-    public function interpret_dir(string $dir): bool {
-        $files = scandir($dir);
-        foreach ($files as $file)
-            if (!in_array($file, ['.', '..']))
-                if (!$this->interpret_file($dir . DIRECTORY_SEPARATOR . $file))
-                    return false;
-        return true;
-    }
-
     public function interpret_file(string $file): bool {
-        if (is_dir($file))
-            return $this->interpret_dir($file);
-
-        $lines = file($file);
-        foreach ($lines as $line)
-            if (!$this->interpret_line($line))
-                return false;  
+        if (is_dir($file)) {
+            $files = scandir($dir = $file);
+            foreach ($files as $file)
+                if (!in_array($file, ['.', '..']))
+                    if (!$this->interpret_file($dir . DIRECTORY_SEPARATOR . $file))
+                        return false;
+        } else {
+            $lines = file($file);
+            foreach ($lines as $line)
+                if (!$this->interpret_line($line))
+                    return false;
+        }  
         return true;
     }
 
