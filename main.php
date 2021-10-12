6299784f0428204420021f8f301b9c0f8a94ef45
SIGUI Kessé Emmanuel (https://sikessem.com) <developer@sikessem.com>
2021-10-13 00:37:21 +0200

    :art: main.php: call main() from cli/skel

--- main.php
+++ main.php
@@ -4,4 +4,7 @@ use Skel\Runtime\Program;
 
 require_once __DIR__ . '/cfg/bootstrap.php';
 
-(new Program(getcwd()))->execute($argv, $argc);
+function main(string $wdir, array $argv, int $argc): void {
+    if (in_array(PHP_SAPI, ['cli', 'phpdbg'], true))
+        (new Program($wdir))->execute($argv, $argc);
+}
